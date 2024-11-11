d902f458deb1ab35c9bd28ac9825f7eb614b6b8e
Teedex1 <opeyemiridwan8965@gmail.com>
2024-11-11 14:58:40 -0600

    fixed a bug

--- src/components/Contact/map/Map.jsx
+++ src/components/Contact/map/Map.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import LocationPin from "./LocationPin"
-import './map.css'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import LocationPin from './LocationPin';
+import './map.css';
 
 function Map({ location, zoomLevel }) {
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   return (
     <div className="map">
-    <h4 className="map-h2">Come Visit Us At Our Office</h4>
+      <h4 className="map-h2">Come Visit Us At Our Office</h4>
 
-    <div className="google-map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyA3Ccjwgo0E1Bg9HrLWeZxLKzUitQ-uhOM' }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-      >
-        <LocationPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        />
-      </GoogleMapReact>
+      <div className="google-map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: googleMapsApiKey }}
+          defaultCenter={location}
+          defaultZoom={zoomLevel}
+        >
+          <LocationPin lat={location.lat} lng={location.lng} text={location.address} />
+        </GoogleMapReact>
+      </div>
     </div>
-  </div>
-  )
-  }
-export default Map
+  );
+}
+
+export default Map;
